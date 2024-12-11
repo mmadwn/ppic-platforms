f980dc8d6e6152c77f85b83cd132a5010911353a
mmadwn <muh.rdwan.u@gmail.com>
2024-12-11 11:54:40 +0700

    f

--- src/components/platform/PlatformCard.tsx
+++ src/components/platform/PlatformCard.tsx
@@ -1,15 +1,12 @@
-import { Card, Tag } from "antd"
+import { Card } from "antd"
 import Image from "next/image"
 import Link from "next/link"
 import { useTheme } from "@/providers/themeProvider"
 import styles from './Card.module.css'
 import { Platform } from "@/types"
-import { getStatusColor } from "@/utils/platformUtils"
-const { Meta } = Card
 
 export function PlatformCard({ title, description, img_url, index_url, status }: Platform) {
   const { theme } = useTheme()
-  const statusColor = getStatusColor(status);
 
   return (
     <Link href={index_url} className={styles.cardContainer}>
